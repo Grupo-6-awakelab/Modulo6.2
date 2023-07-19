0da9a94b8dfe627e42eb9489d1b66302937e3859
Kevin Cabello <keving758@gmail.com>
2023-07-18 21:48:27 -0400

    first commit

---
+++ src/main/java/cl/awakelab/Grupal6M6/Grupal6M6Application.java
@@ -0,0 +1,13 @@
+package cl.awakelab.Grupal6M6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupal6M6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupal6M6Application.class, args);
+	}
+
+}
---
+++ src/test/java/cl/awakelab/Grupal6M6/Grupal6M6ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.awakelab.Grupal6M6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupal6M6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
