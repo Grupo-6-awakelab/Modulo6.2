163f7f161b074b7730c1e38479e592f29b0b56d9
Kevin Cabello <keving758@gmail.com>
2023-07-27 20:02:07 -0400

    commit

---
+++ src/main/java/cl/awakelab/Grupal6M6/web/controller/HomeController.java
@@ -0,0 +1,25 @@
+package cl.awakelab.Grupal6M6.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+
+
+    @GetMapping
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(){
+        return "dashboard";
+    }
+}
+
